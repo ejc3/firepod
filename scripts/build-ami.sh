98b0badeaf4ccdf3be1604a70fb8f0c80cf6868d
ejc3 <ejc3@users.noreply.github.com>
2026-01-04 10:46:51 +0000

    Generate config before kernel install

--- scripts/build-ami.sh
+++ scripts/build-ami.sh
@@ -84,6 +84,9 @@ git clone --depth 1 https://github.com/ejc3/firepod.git /tmp/firepod
 cd /tmp/firepod
 cargo build --release
 
+# Generate default config (needed for kernel profile lookup)
+./target/release/fcvm setup --generate-config --force
+
 # Build and install kernel using fcvm setup
 aws ec2 create-tags --resources $INSTANCE_ID --tags Key=KernelVersion,Value=nested --region us-west-1
 ./target/release/fcvm setup --kernel-profile nested --build-kernels --install-host-kernel
